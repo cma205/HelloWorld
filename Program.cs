ce1df80dd64c8086c715a2962ed33372ffc9f107
Charles Alford <cm205@Charless-MacBook-Pro.local>
2020-04-01 09:28:03 -0500

    Hello World

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The world is a vampire!");
+            Console.WriteLine("Sent to drain?");
+        }
+    }
+}
